3b5fa28f1181679e486789b45bf2d71ecb66d5b6
markaf <markaf.github@gmail.com>
2014-05-22 23:29:01 +0200

    app.factory arreglat / quick-date / guardar ctrls

--- server_PFC_1/server_sqlite.go
+++ server_PFC_1/server_sqlite.go
@@ -25,22 +25,22 @@ func crearBBDD(crear bool) {
 
 		defer db.Close()
 
-		db.Exec("CREATE TABLE controls (tema TEXT, aula TEXT, data TEXT);")
+		db.Exec("CREATE TABLE controls (tema TEXT, aula TEXT, data TEXT, id_control TEXT);")
 		db.Exec("CREATE TABLE proposats (tema TEXT, data TEXT, alumne TEXT);")
-		db.Exec("CREATE TABLE usuaris (nom TEXT, password TEXT, tipus TEXT);")
+		db.Exec("CREATE TABLE usuaris (nom TEXT, password TEXT, tipus TEXT, id_alumne TEXT);")
 		db.Exec("CREATE TABLE temes (tema TEXT, aula TEXT, data TEXT);")
-		db.Exec("CREATE TABLE inscrits (id_alumne INTEGER, id_control INTEGER);")
-		db.Exec("CREATE TABLE notes (id_alumne INTEGER, id_control INTEGER, nota INTEGER);")
+		db.Exec("CREATE TABLE inscrits (id_alumne TEXT, id_control TEXT);")
+		db.Exec("CREATE TABLE notes (id_alumne TEXT, id_control TEXT, nota TEXT);")
 		//trans, err := db.Begin()
 		//if err != nil {
 		//	log.Printf("begin: %v\n", err)
 		//}
-		db.Exec("insert into usuaris(nom, password, tipus) values('marc.andres.fontanet', '12345678', 'alumne');")
-		db.Exec("insert into usuaris(nom, password, tipus) values('pau.fernandez', '12345678', 'professor');")
-		db.Exec("insert into usuaris(nom, password, tipus) values('user', '1234', 'professor');")
+		db.Exec("insert into usuaris(nom, password, tipus) values('alumne', '1234', '0');")
+		db.Exec("insert into usuaris(nom, password, tipus) values('pau.fernandez', '1234', '1');")
+		db.Exec("insert into usuaris(nom, password, tipus) values('admin', '1234', '1');")
 
-		db.Exec("insert into controls(tema, aula, data) values('Objectes + Strings', '2.18', '19/05/2014');")
-		db.Exec("insert into controls(tema, aula, data) values('Getline + While cin', '1.08', '25/05/2014');")
+		db.Exec("insert into controls(tema, aula, data,id_control) values('Objectes + Strings', '2.18', '19/05/2014','21434');")
+		db.Exec("insert into controls(tema, aula, data, id_control) values('Getline + While cin', '1.08', '25/05/2014','49382');")
 		log.Println("insert")
 		/*if err != nil {
 
@@ -74,7 +74,8 @@ func hLogin(w http.ResponseWriter, r *http.Request) {
 	}
 	defer stmt.Close()
 	var (
-		nom, passwd, tipus string
+		nom, passwd string
+		tipus       int
 	)
 	//var name string
 	err = stmt.QueryRow(req["user"]).Scan(&nom, &passwd, &tipus)
@@ -136,11 +137,13 @@ func hAddUser(w http.ResponseWriter, r *http.Request) {
 
 func hAddControl(w http.ResponseWriter, r *http.Request) {
 	log.Println("AddControl")
+	var stat string
+	stat = ""
 	//req := struct{ Title string }{}
 	req := make(map[string]string)
 	json.NewDecoder(r.Body).Decode(&req)
 	log.Println("req passat")
-
+	log.Println(req["tema"])
 	db, err := sql.Open("sqlite3", "./BBDD.db")
 	if err != nil {
 		fmt.Printf("open: %v\n", err)
@@ -148,33 +151,56 @@ func hAddControl(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer db.Close()
-	db.Exec("insert into controls(tema, aula, data) values('" + req["tema"] + "', '" + req["aula"] + "','" + req["data"] + "'');")
-	/*stmt, err := db.Prepare("select nom from usuaris where nom = ?")
+	stmt, err := db.Prepare("select id_control from controls where id_control = ?")
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "error intern", http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
-	var stat string
-	stat = "NOok"
+
 	var nom string
 	//var name string
-	err = stmt.QueryRow(req["user"]).Scan(&nom)
+	err = stmt.QueryRow(req["Id"]).Scan(&nom)
 	if err != nil {
-		db.Exec("insert into usuaris(nom, password, tipus) values('" + req["user"] + "', '" + req["password"] + "', '" + req["tipus"] + "');")
-		log.Println(err)
-		stat = "ok"
+		res, err := db.Query("select data from controls where tema ='" + req["tema"] + "'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("paso1")
+		match := false
+		for res.Next() {
+			var dataCntrl = ""
+			if err := res.Scan(&dataCntrl); err != nil {
+				log.Fatal(err)
+			}
+			if dataCntrl == req["data"] {
+				match = true
+			}
+			//req = append(req, ctrl)
+		}
+		log.Println("paso2")
+		if err := res.Err(); err != nil {
+			log.Fatal(err)
+		}
+		if match == false {
+			db.Exec("insert into controls(tema, aula, data,id_control) values('" + req["tema"] + "', '" + req["aula"] + "','" + req["data"] + "','" + req["Id"] + "');")
+			stat = "ok"
+		} else {
+			stat = "SameData"
+		}
+	} else {
+		stat = "SameID"
 	}
-   */
 
 	json.NewEncoder(w).Encode(map[string]interface{}{
-		// "ok": stat,
+		"stat": stat,
 	})
 }
 
 type Control struct {
 	Tema, Data, Aula string
+	Id               int
 }
 
 func hGetControls(w http.ResponseWriter, r *http.Request) {
@@ -197,7 +223,7 @@ func hGetControls(w http.ResponseWriter, r *http.Request) {
 	log.Println("paso1")
 	for res.Next() {
 		ctrl := Control{}
-		if err := res.Scan(&ctrl.Tema, &ctrl.Aula, &ctrl.Data); err != nil {
+		if err := res.Scan(&ctrl.Tema, &ctrl.Aula, &ctrl.Data, &ctrl.Id); err != nil {
 			log.Fatal(err)
 		}
 		log.Println(ctrl.Tema)
--- server_PFC_1/static/js/main.js
+++ server_PFC_1/static/js/main.js
@@ -1,112 +1,217 @@
 $('.dropdown-toggle').dropdown()
 var d = new Date();
-console.log(d);
+/*console.log(d);
 console.log(localStorage.day);
 console.log("localStorage = " + localStorage.user);
-localStorage.user = (localStorage.user || "");
-if(d > localStorage.day){
-	localStorage.user = "";
-} else {localStorage.user = (localStorage.user || "");}
+//localStorage.user = (localStorage.user || null);
+//localStorage.visible = (localStorage.visible || null);*/
+console.log(localStorage.visible);
 localStorage.day = d;
-var app = angular.module('angular_app',['ngResource', 'ngRoute']);
+var app = angular.module('angular_app',['ngResource', 'ngRoute', "ngQuickDate"]);
+
+
+
+
+
+/*app.config(function(ngQuickDateDefaultsProvider) {
+  // Configure with icons from font-awesome
+  return ngQuickDateDefaultsProvider.set({
+    closeButtonHtml: "<i class='fa fa-times'></i>",
+    buttonIconHtml: "<i class='fa fa-clock-o'></i>",
+    nextLinkHtml: "<i class='fa fa-chevron-right'></i>",
+    prevLinkHtml: "<i class='fa fa-chevron-left'></i>",
+    // Take advantage of Sugar.js date parsing
+    parseDateFunction: function(str) {
+      d = Date.create(str);
+      return d.isValid() ? d : null;
+    }
+  });
+});*/
+
+
+
+
 
 app.config(function($routeProvider) {
    $routeProvider
       .when('/', {
          templateUrl: "login.html",
-         controller: function($scope, $location){
-         	console.log("eeeeeei");
-         	if($scope.user.nom !== ""){
-		         $location.url('/user');
-            }
-     	   }
+        controller: "login_ctrl",
       })
       .when('/login', {
          templateUrl: "login.html",
-         controller: function($scope, $location){
-         	console.log("eeeeeei");
-         	if($scope.user.nom !== ""){
-		         $location.url('/user');
-            }
-     	   }
+         controller: "login_ctrl",
       })
       .when('/user', {
          templateUrl: "user.html",
-         controller: function($scope, $location, $route){
-         	console.log("eeeeeei");
-         	console.log($scope.user.nom);
-         	if($scope.user.nom === ""){
-		         $location.url('/login');
-            }
-     	   }
+         controller: "user_ctrl",
       })
       .when('/afegir_alumnes', {
          templateUrl: "afegir_alumnes.html",
+         controller: "affAlumnes_ctrl",
       })
       .otherwise({
          template: "Página no encontrada",
       });
 });
 
-app.controller('user_ctrl', function ($scope, $resource, $location, $http){
-	if($scope.user.nom !== ""){
-		$location.url('/user');
+
+
+app.factory('User', function($http, $rootScope, $route, $location) {
+	var User = {};
+	if(localStorage.user == "null"){
+		User.nom = null;
+	} else{
+	User.nom = localStorage.user || null;}
+
+	User.Admin = localStorage.visible || null;
+	//User.isAdmin = false;
+
+	User.copyData = function(inf) {
+		console.log("noom: "+inf.nom+" , passwd: " +inf.passwd + ",  tipus: " +inf.tipus)
+      User.Admin = parseInt(inf.tipus);
+      console.log("copyData admin:  " + User.Admin );
+      localStorage.visible = parseInt(inf.tipus);
+      //User.UserID   = data.User.UserID;
+      //User.Contols = data.U
+      User.nom = inf.nom;
+      User.password = inf.passwd;
+   }
+
+   User.getUserName = function(){
+   		return User.nom;
+   }
+
+   User.isLoggedIn = function () {
+      return (User.nom != null || User.nom != undefined);
+   }
+
+   User.isAdmin = function(){
+
+   	return User.Admin;
+   }
+
+   User.isReservar = function(index){
+		/*if(!User.Admin){
+		return $scope.User.controls[].reservat;
+		}else {return false;}
+		*/
 	}
 
+   User.login = function(_user){
+		$http.post('/acces_login',_user).
+		   success(function(data){
+		   	 console.log("noom: "+data.nom+" , passwd: " +data.passwd + ",  tipus: " +data.tipus)
+			  	User.copyData(data);
+			   //	localStorage.user = "holaaaaaa";
+			   	//localStorage.visible = User.tipus;
+			   	localStorage.user = User.nom;
+			  	console.log(localStorage.user);
+				$location.url('/user');
+				//$route.reload();
+		   }).error(function(){
+			   alert("Login failed!");
+			   $route.reload();
+		   });
+		   console.log("islogged in: " + User.isLoggedIn());
+   }
+
+   User.logout = function(){
+   	   User.nom = null;
+      delete User.password;
+      delete User.Admin;
+      delete localStorage.user;
+      console.log(User.nom);
+      console.log("islogged in: " + User.isLoggedIn());
+   }
+   return User;
 });
-app.controller('app_ctrl', function ($scope, $resource, $location, $http, $route) {
-	/*$scope.dades = [
-	  {dni:"43566438E",nivell:"1"},
-	  ];*/
-	$scope.controls_fets = [
+
+app.controller('menu_ctrl', function ($scope, $resource, $location, $http, $route,User) {
+	$scope.user = User;
+	var a = {
+		"isAdmin": function(){
+		return true;
+	}}
+
+	$scope.login_out = function(){
+		User.logout();
+		//$scope.user.nom = "";
+		$scope.inf = {};
+		$location.path('/login');
+	}
+	//console.log(User);
+	$scope.menu = [
+		{name: 'El meu compte', click:'', link: '', visible: a},
+		{name: 'Afegir alumnes',click:'', link: '#/afegir_alumnes', visible: User}
+		//{name: 'Tancar sessió',click: '',link: '#', visible: true}
+	];
+});
+d
+
+app.controller('login_ctrl', function ($scope, $resource, $location, $http, $route,User) {
+	if(User.isLoggedIn() == true){
+         		console.log("holaaaaaa");
+		         $location.url('/user');
+            }
+
+	$scope.inf = {};
+
+	$scope.submit = function(){
+		User.login($scope.inf);
+		$scope.isVisible = User.isAdmin();
+		console.log("login is tipus:  "+ $scope.inf.tipus);
+		console.log("login is Admin:  "+ User.isAdmin()); 
+		//localStorage.visible = User.isAdmin();
+	}	
+});
+
+app.controller('user_ctrl', function ($scope, $resource, $location, $http, $route,User) {
+		console.log("user_ctrl");
+        console.log(User.nom);
+        console.log(User.isLoggedIn());
+        if(!User.isLoggedIn()){
+		     $location.url('/login');
+            }
+
+        console.log("isVisible?: " + User.isAdmin());
+        $scope.isVisible = User.isAdmin();
+        
+
+        $scope.date1 = new Date();
+        console.log("data: "+$scope.date1);
+  
+        $scope.isAdding = false;
+        $scope.reservat = false;
+        $scope.user = {};
+		$scope.user.nom = localStorage.user || "";
+       $scope.controls_fets = [
 		{id:"1",tema:"maps",aula:"2.12", data:"24/05/2014"},
 		{id:"2",tema:"for",aula:"1.18", data:"14/04/2014"},
 		{id:"3",tema:"while",aula:"Per determinar", data:"8/03/2014"},
 	];
 	//$scope.controls_fets = [];
 	$scope.controls_per_fer =[];
-	/*$scope.controls_per_fer = [
-		{id:"1",tema:"maps",aula:"2.12", data:"24/05/2014"},
-		{id:"2",tema:"for",aula:"1.18", data:"14/04/2014"},
-		{id:"3",tema:"while",aula:"Per determinar", data:"8/03/2014"},
-	];*/
+	$scope.affControl = {};
+	$scope.affControl.tema ="";
+	$scope.affControl.data ="";
+	$scope.affControl.aula ="";
+	$scope.affControl.Id ="";
+
+	
+	$scope.onlyWeekdays = function(d) {
+  dayIndex = d.getDay();
+  return ((dayIndex != 0) && (dayIndex != 6));
+}
 
 	$scope.proposats = [
 		{tema: "if/else", data:"30/05/2014"},
 		{tema: "for", data:"28/05/2014"},
 	];
 
-
-	//$scope.reloaded = false;
-
-	$scope.inf = {};
-	$scope.user = {};
-	$scope.user.nom = localStorage.user;
-	$scope.text_ = {};
-	$scope.addUser = {};
-	$scope.addUser.tipus = "alumne";
-	$scope.affControl = {tema:"",data:"",aula:""};
-
-
-
-	$scope.login_out = function(){
-		$scope.user.nom = "";
-		$scope.inf.user = "";
-		$scope.inf.password = "";
-		localStorage.user = "";
-		$location.path('/login');
+	$scope.reservar_control = function(indx){
+		$scope.reservat = !$scope.reservat;
 	}
-
-	
-	$scope.isVisible = (localStorage.visible || true); 
-
-	$scope.menu = [
-		{name: 'holaa', click:'', link: '', visible: true},
-		{name: 'Afegir alumnes',click:'', link: '#/afegir_alumnes', visible: $scope.isVisible}
-		//{name: 'Tancar sessió',click: '',link: '#', visible: true}
-	];
-	console.log("afegir alumnes? : " + $scope.menu[1].visible);
-	console.log("localstorage.visible : " + localStorage.visible);
 
 	$scope.get_controls = function() {
 	console.log("controls demanats");
@@ -130,29 +235,97 @@ app.controller('app_ctrl', function ($scope, $resource, $location, $http, $route
 
 	}
 
-	$scope.submit = function(){
-		console.log($scope.inf);
-		$http.post('/acces_login',$scope.inf).
+	$scope.get_controls();
+
+
+	$scope.proposta = {};
+	$scope.proposta.tema ="tema";
+	$scope.add_proposta = function(){
+		console.log($scope.proposta);
+		return;
+	}
+
+	$scope.temes_proposta =  [
+	{tema:"Getline + While cin"},
+	{tema:"Objectes + Strings"},
+	{tema:"Fitxers + StringStreams"},
+	{tema:"Vectors"},
+	{tema:"Iteradors"},
+	{tema:"Classes"},
+	{tema:"Maps"},
+	{tema:"Llistes"},
+	{tema:"Classes Compostes"},
+	{tema:"Eficiencia 1"},
+	{tema:"Operadors"},
+	{tema:"Eficiencia 2"},
+	];
+  	
+
+	$scope.tema_proposta = function(index){
+		$scope.proposta.tema = $scope.temes_proposta[index].tema;
+	}
+
+	$scope.afegir_control = function() {
+		console.log("id adding: " + $scope.isAdding );
+      if (!$scope.isAdding) {
+         $scope.isAdding = true;
+         console.log($scope.date1.getDay());
+      } else {
+      	if($scope.affControl.tema !="" && $scope.affControl.data !="" && $scope.affControl.aula != ""){
+      		var afegit = false;
+      		while(afegit == false){
+      			afegit = true;
+      			console.log("bucleeeee");
+      		$scope.affControl.Id = Math.round(Math.random()*10000);
+      		$scope.affControl.Id = $scope.affControl.Id.toString();
+      		console.log($scope.affControl)
+      		console.log($scope.affControl.tema);
+      		$scope.cntrl = {};
+      		$scope.cntrl.tema = $scope.affControl.tema;
+      		$scope.cntrl.data = $scope.affControl.data;
+      		$scope.cntrl.aula = $scope.affControl.aula;
+      		$scope.cntrl.Id = $scope.affControl.Id;
+      		console.log($scope.cntrl);
+      		$http.post('/addControl',$scope.cntrl).
 		   success(function(data){
-			   $scope.user.nom = data.nom;
-			   if (data.tipus === "alumne") {
-				   localStorage.user = $scope.user.nom;
-				   $scope.isVisible = false; 
-				   localStorage.visible = false;
-				   console.log("localstorage a false");
-				   console.log("localstorage: "+ localStorage.visible);
-			   } else {
-				   localStorage.user = $scope.user.nom;
-				   console.log(localStorage.user);
-				   $scope.isVisible = true;
-				   localStorage.visible = true;
-		      };
-				$location.url('/user');
+			   console.log(data.stat);
+			   if(data.stat === "ok"){
+			   	console.log("if correcte");
+			   	afegit = true;
+			   	$scope.controls_per_fer.push({tema:$scope.affControl.tema, data:$scope.affControl.data, aula:$scope.affControl.aula, Id: $scope.affControl.Id});
+		$scope.isAdding = false;
+		console.log("Ara si que afegeixo");
+		$scope.affControl.tema = "";
+		$scope.affControl.aula = "";
+		$scope.affControl.data = "";
+			   } else if(data.stat = "SameData") {
+			   		alert("Examen repetit!");
+			   		afegit = true;
+			   	} else { 
+			   		console.log("mareix ID! repetimos?");
+					
+					}
+
 		   }).error(function(){
-			   alert("Login failed!");
+			   alert("error");
 		   });
-	}	
-   
+	}
+	
+		/*$scope.controls_per_fer.push({tema:$scope.affControl.tema, data:$scope.affControl.data, aula:$scope.affControl.aula, Id: $scope.affControl.Id});
+		$scope.isAdding = false;
+		console.log("Ara si que afegeixo");
+		$scope.affControl.tema = "";
+		$scope.affControl.aula = "";
+		$scope.affControl.data = "";*/
+		}
+         
+         
+      }
+	}
+
+	$scope.cancelar_affControl = function(){
+   		$scope.isAdding = false;
+   }
 
 	$scope.delete_exam = function(exam){
 		var r = confirm("Estàs segur?");
@@ -161,67 +334,65 @@ app.controller('app_ctrl', function ($scope, $resource, $location, $http, $route
   		}  
 	}
 
-	$scope.get_controls();
+	});
 
-   $scope.isAdding = false;
-
-   $scope.cancelar_affControl = function(){
-   		$scope.isAdding = false;
-   }
-	$scope.afegir_control = function() {
-		console.log("id adding: " + $scope.isAdding );
-      if (!$scope.isAdding) {
-         $scope.isAdding = true;
-      } else {
-      		/*$http.post('/addControl',$scope.addUser).
-		   success(function(data){
-			   console.log(data.ok);
-			   console.log(data.ok === "ok");
-			   if(data.ok === "ok"){
-				   console.log("oleeee");
-				   $("#resposta_addUser").html("L'usuari s'ha afegit correctament");
-				   $("#resposta_addUser").css( "color", "green" );
-			   } else {
-					console.log("cacaaaaa");
-					$("#resposta_addUser").html("Aquest usuari ja existeix");
-					$("#resposta_addUser").css( "color", "red" );
-				}
-
-		   }).error(function(){
-			   alert("error");
-		   });*/
+app.controller('affAlumnes_ctrl', function ($scope, $resource, $location, $http, $route,User) {
 	
-	if($scope.affControl.tema !="" && $scope.affControl.data !="" && $scope.affControl.aula != ""){
-		$scope.controls_per_fer.push({tema:$scope.affControl.tema, data:$scope.affControl.data, aula:$scope.affControl.aula});
-		$scope.isAdding = false;
-		console.log("Ara si que afegeixo");
-		$scope.affControl.tema = "";
-		$scope.affControl.aula = "";
-		$scope.affControl.data = "";
-		}
-         
-         
-      }
+	$scope.addUser = {};
+	$scope.addUser.tipus = 0;
+	$scope.tipus = "Privilegis"
+	/*$scope.user = {};
+	$scope.user.nom = localStorage.user || "";*/
+	$scope.admin = function(){
+		console.log("Professor!!!");
+		$scope.tipus = "Professor";
+		$scope.addUser.tipus = 1;
 	}
 
+	$scope.alumne = function(){
+		console.log("Alumne!!!");
+		$scope.tipus = "Alumne";
+		$scope.addUser.tipus = 0;
+	}
 	$scope.add_user = function(){
-		$http.post('/addUser',$scope.addUser).
-		   success(function(data){
-			   console.log(data.ok);
-			   console.log(data.ok === "ok");
-			   if(data.ok === "ok"){
-				   console.log("oleeee");
-				   $("#resposta_addUser").html("L'usuari s'ha afegit correctament");
-				   $("#resposta_addUser").css( "color", "green" );
-			   } else {
-					console.log("cacaaaaa");
-					$("#resposta_addUser").html("Aquest usuari ja existeix");
-					$("#resposta_addUser").css( "color", "red" );
-				}
+		var a = false;
+		if($scope.tipus == "Professor" ||$scope.tipus == "professor"){
+			$scope.addUser.tipus = 1;
+			a = true;
+		}
+		if($scope.tipus == "Alumne" ||$scope.tipus == "alumne"){
+			$scope.addUser.tipus = 0;
+			a = true;
+		}
+		if(a == true){
+			console.log($scope.addUser);
+			$scope.affUser = {};
+			$scope.affUser.user = $scope.addUser.user;
+			$scope.affUser.password = $scope.addUser.password;
+			$scope.affUser.tipus = $scope.addUser.tipus.toString();
+			$http.post('/addUser',$scope.affUser).
+		   	success(function(data){
+			   	console.log(data.ok);
+			   	console.log(data.ok === "ok");
+			   	if(data.ok === "ok"){
+				   	console.log("oleeee");
+				   	$("#resposta_addUser").html("L'usuari s'ha afegit correctament");
+				   	$("#resposta_addUser").css( "color", "green" );
+				   	$scope.addUser.user = "";
+				   	$scope.addUser.password = "";
+			   	} else {
+						console.log("cacaaaaa");
+						$("#resposta_addUser").html("Aquest usuari ja existeix");
+						$("#resposta_addUser").css( "color", "red" );
+					}
 
-		   }).error(function(){
-			   alert("error");
-		   });
+		   	}).error(function(){
+			   	alert("error");
+		   	});
+		} else {
+			$("#resposta_addUser").html("Aquest tipus d'usuari no existeix");
+			$("#resposta_addUser").css( "color", "red" );
+		  }
 	}
 }); 
 
@@ -229,5 +400,13 @@ app.directive("proposats", function() {
    return {
       restrict: "E",
       templateUrl: 'proposats.html'
+   };
+});
+
+
+app.directive("proposar", function() {
+   return {
+      restrict: "E",
+      templateUrl: 'proposar.html'
    };
 });
